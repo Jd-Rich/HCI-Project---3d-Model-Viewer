20a3ddd4cc061e5d7b06167860bb0b592fed5af5
JD Rich <jdrich@comcast.net>
2020-10-19 21:47:32 -0400

    Initial commit, with needed files.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import com.sun.j3d.utils.geometry.*;
+import com.sun.j3d.utils.universe.*;
+import com.sun.j3d.utils.image.TextureLoader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+
+    }
+}
