9d99bc809cc7f1f916500f5ee90460082b0901ae
JD Rich <jdrich@comcast.net>
2020-11-12 19:48:32 -0500

    Fixed the duplicate frame issue.

--- src/Panel.java
+++ src/Panel.java
@@ -1,6 +1,7 @@
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.FPSAnimator;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -12,26 +13,34 @@ import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 
 public class Panel  extends Frame{
-    private JButton changeShape = new JButton("Change Shape");
+    //Shapes
     Cube cube = new Cube();
+    Pyramid pyramid = new Pyramid();
 
+    //Buttons for the frame
+    private JButton changeShape = new JButton("Change Shape");
+
+    //GL setup
+    GLProfile profile = GLProfile.get(GLProfile.GL2);
+    GLCapabilities capabilities = new GLCapabilities(profile);
+    GLCanvas glcanvas = new GLCanvas(capabilities);
+
+    //The GUI
+    Frame gui = new Frame("Frame");
 
     public Component getGUI() {
-        //GL setup
-        GLProfile profile = GLProfile.get(GLProfile.GL2);
-        GLCapabilities capabilities = new GLCapabilities(profile);
-        GLCanvas glcanvas = new GLCanvas(capabilities);
         glcanvas.addGLEventListener(cube);
         glcanvas.setSize(400, 400);
 
-
-        Frame gui = new Frame("Frame");
         gui.setSize(600,600);
         gui.add(glcanvas);
         gui.setVisible(true);
+
         JPanel buttons = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 15));
         gui.add(buttons, BorderLayout.PAGE_END);
 
+        final FPSAnimator animator = new FPSAnimator(glcanvas, 300, true);
+        animator.start();
 
         //Close the window when the X is clicked
         gui.addWindowListener(new WindowAdapter() {
@@ -54,6 +63,17 @@ public class Panel  extends Frame{
 
     //methods
     public void changeShape_ActionPerformed(ActionEvent e) {
+        glcanvas.addGLEventListener(pyramid);
+        glcanvas.setSize(400, 400);
 
+        gui.setSize(600,600);
+        gui.add(glcanvas);
+        gui.setVisible(true);
+
+        JPanel buttons = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 15));
+        gui.add(buttons, BorderLayout.PAGE_END);
+
+        final FPSAnimator animator = new FPSAnimator(glcanvas, 300, true);
+        animator.start();
     }
 }
